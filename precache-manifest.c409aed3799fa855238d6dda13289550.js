f24a0ee9357ed390082714ed2db49bb15a8b6364
Rajendra Kulkarni <frankly.coder@gmail.com>
2021-10-20 00:15:54 +0530

    Updates

---
+++ precache-manifest.c409aed3799fa855238d6dda13289550.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96fd74065bbfdf4629ee389a807b9bc",
+    "url": "/rpod/index.html"
+  },
+  {
+    "revision": "7da5ccbbb4f77c8ee5e2",
+    "url": "/rpod/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e29d20b4c78cb9384fc9",
+    "url": "/rpod/static/css/main.ef95774c.chunk.css"
+  },
+  {
+    "revision": "7da5ccbbb4f77c8ee5e2",
+    "url": "/rpod/static/js/2.197effc5.chunk.js"
+  },
+  {
+    "revision": "3afc3f7b055b07aac8edf61359703488",
+    "url": "/rpod/static/js/2.197effc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29d20b4c78cb9384fc9",
+    "url": "/rpod/static/js/main.9c25d1aa.chunk.js"
+  },
+  {
+    "revision": "a63a3997a9061b8db7ea",
+    "url": "/rpod/static/js/runtime-main.05201165.js"
+  },
+  {
+    "revision": "be86cbb8d659396fc50f8fe036dd39d5",
+    "url": "/rpod/static/media/modernLone.be86cbb8.mp3"
+  }
+]);
